5c3c8121fbc7cc94ec0051c7dd7c8850d22ec861
azeemshaikh38 <azeemshaikh38@gmail.com>
2016-05-25 05:02:53 +0000

    v3

--- Commons.py
+++ Commons.py
@@ -3,4 +3,4 @@ import logging
 class Commons:
     LOGGER_FORMAT = "%(asctime)s %(name)s:%(levelname)s %(pathname)s:%(lineno)s - %(message)s"
     LOGGER_LEVEL = logging.INFO
-    LOG_PATH = 'logs/run.log'
+    LOG_FILE = 'logs/run.log'
